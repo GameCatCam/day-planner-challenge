95509237fc864a814c75f7ee94efbc16c0f2c273
GameCatCam <cameronrawlins11@gmail.com>
2023-07-30 16:02:50 -0400

    added planner color functionality

--- script.js
+++ script.js
@@ -16,17 +16,31 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
-  //
+  var timeBlock = $('.time-block')
+  console.log($('#9').attr('id') > dayjs().format('H'))
+  console.log(9 > dayjs().format('H'))
+  console.log('true' > 15)
+
+  timeBlock.each(function() {
+    if ($(this).attr('id') < dayjs().format('H')) {
+      $(this).addClass('past')
+    } else if ($(this).attr('id') > dayjs().format('H')) {
+      $(this).addClass('future')
+    } else {
+      $(this).addClass('present')
+    }
+  })
+  
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
+  
   // TODO: Add code to display the current date in the header of the page.
 
   window.onload = setInterval(function() {
